00e1b7901aac144950fcf50c75ff0dc5906563d2
Dmitry Tarakanovskiy <62096584+DeanRiot@users.noreply.github.com>
2022-06-10 23:52:38 +0300

    8 add database models and context for ef (#11)
    * EF configured

---
+++ notifier/src/EF/Entity/NotifyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace notifier.EF.Entity
+{
+    public class NotifyContext:DbContext
+    {
+        public NotifyContext(DbContextOptions options):base(options) { }
+        public virtual DbSet<SMS> SMS { get; set; }
+        public virtual DbSet<MAIL> MAIL{ get; set; }
+       
+    }
+}
---
+++ notifier/src/EF/Migrations/20220610202644_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace notifier.EF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MAIL",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    mail = table.Column<string>(type: "text", nullable: true),
+                    enabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SMS",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: true),
+                    enabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MAIL");
+
+            migrationBuilder.DropTable(
+                name: "SMS");
+        }
+    }
+}
