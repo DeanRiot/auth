68d72d3f204c016110c4e8f77d0dd5335ce3bd41
= <=>
2022-06-11 15:44:30 +0300

    add communication table model

---
+++ notifier/src/EF/Entity/Communication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace notifier.EF.Entity
+{
+    public class Communication
+    {
+        [Key]
+        public Guid id { get; set; }
+        public string method { get; set; }
+        public Guid user { get; set; }
+        public string status { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
